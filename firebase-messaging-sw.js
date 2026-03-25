11901a5e100cca3341063ec1a83c82a321be5133
Rakesh Lakhara <rakeshlakhara432@github.com>
2026-03-25 16:52:33 +0530

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "lakhara-news.firebaseapp.com",
+  projectId: "lakhara-news",
+  storageBucket: "lakhara-news.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/lakhara.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
